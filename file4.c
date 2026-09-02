edd83c510a9ead0d8c5c67b04d71c763df0ea658
xk-qgdx <2683271682@qq.com>
2026-09-02 20:41:26 +0800

    错误修复：结构体与结构体基础链表

--- file4.c
+++ file4.c
@@ -5,7 +5,7 @@ typedef struct student{
     int score;
 }stu;
 int main(){
-    stu.s1={100;"zhangsan",98};
+    stu.s1={100，"zhangsan",98};
     stu *p=&s1;
     printf("学号：%d,姓名：%s,分数：%d\n",p->id,p->name,p->score);
     return 0;
